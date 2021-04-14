79d5138f5f6ef1ac48eaa01a65ec9120ec22cafd
jgagnon44 <56738211+jgagnon44@users.noreply.github.com>
2021-04-14 16:21:03 -0400

    Do not include the who's and when's in hashcode.

--- src/main/java/com/fossfloors/taskapp/backend/entity/AbstractEntity.java
+++ src/main/java/com/fossfloors/taskapp/backend/entity/AbstractEntity.java
@@ -91,11 +91,11 @@ public abstract class AbstractEntity {
   public int hashCode() {
     final int prime = 31;
     int result = 1;
-    result = prime * result + ((createdBy == null) ? 0 : createdBy.hashCode());
-    result = prime * result + ((dateCreated == null) ? 0 : dateCreated.hashCode());
-    result = prime * result + ((dateModified == null) ? 0 : dateModified.hashCode());
+    // result = prime * result + ((createdBy == null) ? 0 : createdBy.hashCode());
+    // result = prime * result + ((modifiedBy == null) ? 0 : modifiedBy.hashCode());
+    // result = prime * result + ((dateCreated == null) ? 0 : dateCreated.hashCode());
+    // result = prime * result + ((dateModified == null) ? 0 : dateModified.hashCode());
     result = prime * result + ((id == null) ? 0 : id.hashCode());
-    result = prime * result + ((modifiedBy == null) ? 0 : modifiedBy.hashCode());
     return result;
   }
 
