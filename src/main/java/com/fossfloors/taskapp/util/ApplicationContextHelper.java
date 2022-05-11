574b67606504deb3b6fa414e8f9bf2cbcb8227cd
jgagnon44 <56738211+jgagnon44@users.noreply.github.com>
2022-05-11 16:47:37 -0400

    Create ApplicationContextHelper.java

---
+++ src/main/java/com/fossfloors/taskapp/util/ApplicationContextHelper.java
@@ -0,0 +1,26 @@
+package com.fossfloors.taskapp.util;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationContextHelper {
+
+  private static ApplicationContextHelper instance;
+
+  @Autowired
+  private ApplicationContext              applicationContext;
+
+  @PostConstruct
+  public void registerInstance() {
+    instance = this;
+  }
+
+  public static <T> T getBean(Class<T> clazz) {
+    return instance.applicationContext.getBean(clazz);
+  }
+
+}
