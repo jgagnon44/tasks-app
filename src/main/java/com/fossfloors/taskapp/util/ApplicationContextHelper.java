94c0a8cb22374cea38787afeeed0ee96fbfcd3ac
jgagnon44 <56738211+jgagnon44@users.noreply.github.com>
2022-05-12 08:43:07 -0400

    Add javadoc.

--- src/main/java/com/fossfloors/taskapp/util/ApplicationContextHelper.java
+++ src/main/java/com/fossfloors/taskapp/util/ApplicationContextHelper.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+/**
+ * Helper class to access Spring application context.
+ */
 @Component
 public class ApplicationContextHelper {
 
@@ -15,10 +18,17 @@ public class ApplicationContextHelper {
   private ApplicationContext              applicationContext;
 
   @PostConstruct
-  public void registerInstance() {
+  private void registerInstance() {
     instance = this;
   }
 
+  /**
+   * Returns an instance of the requested bean object.
+   * 
+   * @param <T>   - the bean type.
+   * @param clazz - the bean class.
+   * @return the requested object.
+   */
   public static <T> T getBean(Class<T> clazz) {
     return instance.applicationContext.getBean(clazz);
   }
